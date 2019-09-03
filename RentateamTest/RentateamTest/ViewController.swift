588d836e6d44156936abe5e4dd0592c8588186c1
Виктор Григорьев <viktorgrigorev@MacBook-Pro-Viktor.local>
2019-09-03 16:16:58 +0300

    Initial commit

---
+++ RentateamTest/RentateamTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RentateamTest
+//
+//  Created by Виктор Григорьев on 03/09/2019.
+//  Copyright © 2019 Виктор Григорьев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
